e3bbbcc3612e3ac1f6804efb74be52a8d21b0420
Juan-Tello13 <juanluis.tello2002@gmail.com>
2025-08-18 23:16:12 -0300

    Codigo sin adapter

--- patronAdapter/codigoSinAdapter/Program.cs
+++ patronAdapter/codigoSinAdapter/Program.cs
@@ -10,7 +10,42 @@ namespace codigoSinAdapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hola");
+            Temperatura temperatura1 = new Temperatura(30, "celsius");
+            Temperatura temperatura2 = new Temperatura(150, "farenheit");
+            Temperatura temperatura3 = new Temperatura(20, "celsius");
+            Temperatura temperatura4 = new Temperatura(250, "farenheit");
+
+            ///// Se declaran 4 temperaturas con distintas unidades y se agregan a una lista
+
+            List<Temperatura> temperaturas = new List<Temperatura>();
+            temperaturas.Add(temperatura1);
+            temperaturas.Add(temperatura2);
+            temperaturas.Add(temperatura3);
+            temperaturas.Add(temperatura4);
+
+            Console.WriteLine("Lista original: \n");
+            foreach (Temperatura temperatura in temperaturas)
+            {
+                Console.WriteLine($"{temperatura.Valor}, {temperatura.Unidad}");
+            }
+
+            ListaTemperaturas lista = new ListaTemperaturas(temperaturas, "celsius");
+            // Se instancia una lista de temperaturas a partir de la lista anterior
+
+            Console.WriteLine("\n\nLista del objeto instanciado: \n");
+            lista.MostrarTemperaturas();
+
+            //Para poder pasarle todas las temperaturas de la lista, primero hay que convertirlas a una misma unidad
+            foreach (Temperatura temperatura in temperaturas)
+            {
+                temperatura.ConvertirCelsius();
+            }
+
+            //Instanciamos una nueva lista a partir de la lista modificada
+            ListaTemperaturas nuevaLista = new ListaTemperaturas(temperaturas, "celsius");
+            Console.WriteLine("\n\nLista de la nueva instancia:\n");
+            nuevaLista.MostrarTemperaturas();
+            Console.ReadKey();
         }
     }
 }
---
+++ patronAdapter/codigoSinAdapter/Temperatura.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigoSinAdapter
+{
+    internal class Temperatura
+    {
+        private float _valor;
+        private string _unidad;
+
+        public Temperatura(float valor, string unidad)
+        {
+            _valor = valor;
+            _unidad = unidad;
+        }
+        public float Valor
+        {
+            get { return _valor; }
+            set { _valor = value; }
+        }
+        public string Unidad
+        {
+            get { return _unidad; }
+            set { _unidad = value; }
+        }
+
+        public Temperatura ConvertirCelsius()
+        {
+            if (this._unidad == "farenheit")
+            {
+                this._valor = (_valor - 32) * 5 / 9;
+                this._unidad = "celsius";
+
+                return this;
+            }
+            else
+            {
+                return this;
+            }
+        }
+
+        public Temperatura ConvertirFarenheit()
+        {
+            if (this._unidad == "celsius")
+            {
+                this._valor = (_valor * 9 / 5) + 32;
+                this._unidad = "farenheit";
+
+                return this;
+            }
+            else { return this; }
+        }
+    }
+
+}
